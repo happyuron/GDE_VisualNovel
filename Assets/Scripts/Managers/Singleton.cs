a67ba23bb5f8a4aac5c5435cb8df61b349842af8
HAPPYURON <ire031226@gmail.com>
2022-10-06 13:11:01 +0900

    Update Manager System

--- Assets/Scripts/Managers/Singleton.cs
+++ Assets/Scripts/Managers/Singleton.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class Singleton<T> : MonoBehaviour where T: Singleton<T>
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
 {
     public static T Instance;
 
-    void Start()
-    {
+    [SerializeField] private bool dontDestroyOnLoad;
 
+    protected virtual void Awake()
+    {
+        if (FindObjectsOfType<T>().Length > 1)
+            Destroy(gameObject);
+
+        if (dontDestroyOnLoad)
+            DontDestroyOnLoad(gameObject);
+
+        if (Instance == null)
+            Instance = GetComponent<T>();
     }
 
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/Managers/TextManager.cs
+++ Assets/Scripts/Managers/TextManager.cs
@@ -2,15 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TextManager : MonoBehaviour
+public class TextManager : Singleton<TextManager>
 {
-    void Start()
+    public Dictionary<int, string[]> textDic = new Dictionary<int, string[]>();
+
+    public int CurIndex { get; set; }
+
+    public int CurKey { get; set; }
+
+    protected override void Awake()
     {
-        
+        base.Awake();
+        CurIndex = 0; CurKey = 0;
+        textDic.Add(0, new string[] { "평화로운 어느 기숙사", "???: 아... 오늘 뭐먹지?" });
     }
 
-    void Update()
+
+    public string GetText(int _key, int _index)
     {
-        
+        return textDic[_key][_index];
     }
+
+
 }
